a1238771dc80982d13bd22da020b70f6d8c25d5e
hackhim18 <triphimanshu18@gmail.com>
2021-11-30 23:57:24 +0530

    c5

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,51 +4,28 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
-//import mapStyles from '../../mapStyles';
 import useStyles from './styles.js';
 
 const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
-  const matches = useMediaQuery('(min-width:600px)');
+  const isMobile = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
+
+  const coordinates = { lat: 0, lng: 0};
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyA9Nn73waRO6jBAA-vk40vMATB3mgGzPwk'}}
-        defaultCenter={coords}
-        center={coords}
+        bootstrapURLKeys={{ key:'AIzaSyAVUgL7yeeV_BsjVo8j0Jq0AJCWnSfZpvU'}}
+        defaultCenter={coordinates}
+        center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={' '}
-        onChange= {' '}
-        onChildClick={' '}
+        options={''}
+        onChange= {''}
+        onChildClick={''}
       >
-        {places.length && places.map((place, i) => (
-          <div
-            className={classes.markerContainer}
-            lat={Number(place.latitude)} 
-            lng={Number(place.longitude)}
-            key={i}
-          >
-            {!matches
-              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
-              : (
-                <Paper elevation={3} className={classes.paper}>
-                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
-                  <img
-                    className={classes.pointer}
-                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
-                  />
-                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
-                </Paper>
-              )}
-          </div>
-        ))}
-        {weatherData?.list?.length && weatherData.list.map((data, i) => (
-          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
-            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
-          </div>
-        ))}
+        
+        
       </GoogleMapReact>
     </div>
   );
